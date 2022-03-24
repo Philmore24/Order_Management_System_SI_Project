5425e8de736036fd0d16fbfb92be5e8500fdc476
Philmore Foster <fosterphilmore@outlook.com>
2022-03-24 17:07:33 -0500

    Add project files.

---
+++ OrderManagementSystem_SI_Project/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OrderManagementSystem_SI_Project
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
